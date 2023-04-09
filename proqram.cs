381d5e8298dc9e5c70766462020d4a1be0d761cb
PashaAllahverdiyev <pashama@code.edu.az>
2023-04-09 19:24:00 +0400

    proqram.cs

---
+++ proqram.cs
@@ -0,0 +1,37 @@
+﻿class student
+{
+    public string name;
+    public string surname;
+    public string group;
+    public int point;
+    public bool isGraduated;
+
+    public student(string name, string surname, string group, int point, bool isGraduated)
+    {
+        this.name = name;
+        this.surname = surname;
+        this.group = group;
+        this.point = point;
+        this.isGraduated = isGraduated;
+    }
+    public void GetFullInfo() 
+    {
+        Console.WriteLine($"{this.name},{this.surname},{this.group},{this.point}");
+        if (point>50)
+        {
+            Console.WriteLine("mezun oldunuz");
+        }
+        else
+        {
+            Console.WriteLine("mezun olmayib");
+        }
+        if(isGraduated=true &&  point > 80)
+        {
+            Console.WriteLine("ikinci imtahana gire biler");
+        }
+        else if (isGraduated=true && point < 80)
+        {
+            Console.WriteLine("ikinci imtahana gire bilmez");
+        }
+    }
+}
