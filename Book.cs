714df01c2961dbe4e04ef069baa2966b32a83662
PashaAllahverdiyev <pashama@code.edu.az>
2023-04-13 12:12:43 +0400

    Book.cs

---
+++ Book.cs
@@ -0,0 +1,14 @@
+﻿
+namespace App.Core.Models
+{
+   public class Book
+    {
+        public string name { get; set; }
+        public string price { get; set; }
+
+        public void GetFullInfo()
+        {
+            Console.WriteLine(name+" "+price);
+        }
+    }
+}
